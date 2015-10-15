d59912c700c7a18133325997e2d7e89257a34442
Matt Beckett <beck24@gmail.com>
2015-10-15 09:28:20 -0700

    fix notice on plugin setting save

--- actions/views_counter/settings/save.php
+++ actions/views_counter/settings/save.php
@@ -44,7 +44,7 @@ else {
 // Checking which types were removed for the admin right now and include them in the remove_views_counter array
 foreach ($previous_types as $previous_type) {
 	// If the type was removed right now and It was not already added as a removed type then let's add It now
-	if ((!in_array($previous_type, $params['value'])) && (!in_array($previous_type, $removed_types))) {
+	if ((!in_array($previous_type, $params['add_views_counter'])) && (!in_array($previous_type, $removed_types))) {
 		$removed_types[] = $previous_type;
 	}
 }
